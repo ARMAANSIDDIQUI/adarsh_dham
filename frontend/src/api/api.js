8ed5549d6b2db9e58c65f5718339f9896bd08524
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-11-05 21:16:05 +0530

    Latest

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,35 +1,25 @@
 import axios from 'axios';
-// We MUST import store from here to break the cycle.
-import { store } from '../redux/store'; 
+
+const API_URL = process.env.REACT_APP_BACKEND_URL || ''; 
 
 const api = axios.create({
-  // Your backend URL from .env or default
-  baseURL: process.env.REACT_APP_API_URL || '',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    baseURL: `${API_URL}/api`, 
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
-/**
- * THE FIX IS HERE
- * We use an interceptor to add the auth token to every request.
- *
- * By getting the store from the interceptor's scope, we wait
- * until the store has been safely created.
- */
 api.interceptors.request.use(
-  (config) => {
-    // Get the token from the already-loaded store
-    const token = store.getState().auth.token;
-
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers['x-auth-token'] = token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 export default api;
