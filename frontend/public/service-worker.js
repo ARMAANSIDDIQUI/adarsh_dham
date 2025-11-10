ee83e5da27719fd451231456068d61528dc66ef8
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-11-10 19:34:26 +0530

    Notification Redirect

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -8,3 +8,10 @@ self.addEventListener('push', event => {
         })
     );
 });
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
