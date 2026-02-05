52aa19641899878c38433905d31edee65e042029
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2026-02-05 15:10:27 +0530

    build

---
+++ backend/build/service-worker.js
@@ -0,0 +1,33 @@
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    
+    const options = {
+        body: data.body,
+        icon: data.icon || '/VM401196.png', // Default icon with corrected casing
+        badge: '/VM401196.png', // Small icon for the status bar
+        image: data.image, // Big image content if provided
+        vibrate: [200, 100, 200], // Vibration pattern
+        tag: 'adarsh-dham-notification', // Grouping tag
+        renotify: true, // Vibrate/sound even if replacing an old notification
+        data: {
+            url: data.url || '/' // Store the URL to open on click
+        },
+        sound: '/notification.mp3' // Sound file (needs to be in public folder)
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
+});
+
+// PWA Requirement: Functional fetch handler
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
