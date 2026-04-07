56192a9447435c865fb6f4117e3c580c41456ac1
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2026-04-07 23:32:28 +0530

    build errors

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const API_URL = process.env.REACT_APP_API_BASE_URL || '';
+const API_URL = process.env.REACT_APP_API_BASE_URL || '/_/backend';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: `${API_URL}/api`,
